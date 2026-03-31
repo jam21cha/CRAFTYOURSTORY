6ae4d18e4501131827f5429125ff629b3d83f8ef
jam21cha <jamaichajane.gendoy@evsu.edu.ph>
2026-03-31 19:00:31 +0800

    Update CVmodel.php

--- CraftYourStory/model/CVmodel.php
+++ CraftYourStory/model/CVmodel.php
@@ -1,9 +1,26 @@
 <?php
+ 
 class CVModel {
-
-    public function processData($data) {
+ 
+    public function processData(array $data): array {
+        foreach (['name', 'jobtitle', 'email', 'phone', 'location', 'skills', 'education', 'photo'] as $field) {
+            if (isset($data[$field])) {
+                $data[$field] = htmlspecialchars_decode(strip_tags(trim($data[$field])));
+            }
+        }
+ 
+        if (!empty($data['experiences']) && is_array($data['experiences'])) {
+            foreach ($data['experiences'] as &$exp) {
+                foreach (['title', 'company', 'start', 'end', 'date', 'desc'] as $field) {
+                    if (isset($exp[$field])) {
+                        $exp[$field] = htmlspecialchars_decode(strip_tags(trim($exp[$field])));
+                    }
+                }
+            }
+            unset($exp);
+        }
+ 
         return $data;
     }
-
 }
 ?>
